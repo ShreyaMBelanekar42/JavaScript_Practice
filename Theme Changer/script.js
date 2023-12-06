73c5c384cb75e823a317ff529278b5c167be87ba
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2023-12-06 19:18:18 +0530

    complete theme changer

--- Theme Changer/script.js
+++ Theme Changer/script.js
@@ -1,1 +1,53 @@
-console.log("Theme changer");
+const buttons = document.querySelectorAll("button");
+const body = document.querySelector("body");
+
+buttons.forEach(function selectBg(button) {
+  // console.log(button);
+  button.addEventListener("click", function (e) {
+    // console.log(e);
+    // console.log(e.target);
+    // if (e.target.id == "green") {
+    //     body.style.backgroundColor = e.target.id;
+    // } else if(e.target.id == "red"){
+    //     body.style.backgroundColor = e.target.id;
+    // }else if(e.target.id == "pink"){
+    //     body.style.backgroundColor = e.target.id;
+    // }else if(e.target.id == "orange"){
+    //     body.style.backgroundColor = e.target.id;
+    // }else if(e.target.id == "grey"){
+    //     body.style.backgroundColor = e.target.id;
+    // }else if(e.target.id == "purple"){
+    //     body.style.backgroundColor = e.target.id;
+    // }
+
+    switch (e.target.id) {
+      case "green":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      case "red":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      case "pink":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      case "orange":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      case "grey":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      case "purple":
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      default:
+        body.style.backgroundColor = "blue";
+        break;
+    }
+  });
+});
